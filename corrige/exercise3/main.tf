8ab7a4ba84977b0439baa61f3fec4f8c0929fb80
vanessakovalsky <v.david@kovalibre.com>
2022-06-13 17:14:21 +0200

    Add data source instance type

--- corrige/exercise3/main.tf
+++ corrige/exercise3/main.tf
@@ -5,8 +5,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_ec2_instance"{
-    ami = var.AWS_AMI
-    instance_type = "t2.micro"
+    ami = data.aws_ami.ubuntu-ami.id
+    instance_type = tolist(data.aws_ec2_instance_types.ami_instance.instance_types)[0]
     vpc_security_group_ids = [aws_security_group.instance_sg.id]
     key_name = aws_key_pair.maclessh.key_name
     user_data = <<-EOF
@@ -56,4 +56,21 @@ resource "aws_key_pair" "maclessh" {
 
 output "adresse_ip_instance" {
   value = aws_instance.my_ec2_instance.public_ip
+}
+
+data "aws_ami" "ubuntu-ami"{
+    owners = ["099720109477"]
+    most_recent = true
+    
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20200408"]
+    }
+}
+
+data "aws_ec2_instance_types" "ami_instance" {
+    filter {
+      name = "processor-info.supported-architecture"
+      values = [data.aws_ami.ubuntu-ami.architecture]
+    }
 }
